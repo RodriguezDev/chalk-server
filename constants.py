19051d3a653f7bd09de57cc3bd863555b803d2da
Chris Rodriguez <chrismail627@gmail.com>
2022-08-29 23:18:30 -0700

    First commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+API values
+
+Get these by signing up for a Twitter dev account @
+https://developer.twitter.com/en/portal/petition/essential/basic-info
+"""
+BEARER_TOKEN = "xyz"
+
+USER_ID_TO_GET_LIKES_FROM = 123
---
+++ main.py
@@ -0,0 +1,9 @@
+# It's chalk stalk time.
+
+import constants
+
+def main():
+    print(constants.BEARER_TOKEN)
+
+if __name__ == '__main__':
+    main()
