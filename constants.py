d8cff4e92b9bca5ea26114d8e1dc38c94e71aa8b
Chris Rodriguez <chrismail627@gmail.com>
2022-09-01 23:55:16 -0700

    Adding constants.py back

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""
+API values
+
+Get these by signing up for a Twitter dev account @
+https://developer.twitter.com/en/portal/petition/essential/basic-info
+"""
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOtbdAEAAAAAvqy8n3t0H4JQjU0cLPUbca5lZSk%3DSmj4Gy6s8uBwMJTXcm9RdEcEwGoZHQApxHhFrNoCaRuti1t31L"
+
+USERNAME_TO_GET_LIKES_FROM = "chokieeikohc"
+TWEETS_PER_REQUEST = 100
